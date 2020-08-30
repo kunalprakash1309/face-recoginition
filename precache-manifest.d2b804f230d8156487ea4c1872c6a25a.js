300b0f361679b175fd57f3882cd914baf9db9519
kunal prakash <kunalprakash1309@gamil.com>
2020-08-31 01:02:19 +0530

    Updates

---
+++ precache-manifest.d2b804f230d8156487ea4c1872c6a25a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3546dc0de5ba1f98bf6416c0026df8e9",
+    "url": "/face-recoginition/index.html"
+  },
+  {
+    "revision": "1dafb4ce437561f89841",
+    "url": "/face-recoginition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ac277ecf8292d113b28b",
+    "url": "/face-recoginition/static/css/main.c4ecbbf0.chunk.css"
+  },
+  {
+    "revision": "1dafb4ce437561f89841",
+    "url": "/face-recoginition/static/js/2.bca1711b.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/face-recoginition/static/js/2.bca1711b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac277ecf8292d113b28b",
+    "url": "/face-recoginition/static/js/main.2bbb5a04.chunk.js"
+  },
+  {
+    "revision": "4d0765634c5cf3f55481",
+    "url": "/face-recoginition/static/js/runtime-main.af826636.js"
+  }
+]);
